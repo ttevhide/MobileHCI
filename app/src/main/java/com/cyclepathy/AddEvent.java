d1c249db6633d446aae24fd8451169f921674a48
ttevhide <ttevhide@gmail.com>
2019-03-13 15:26:19 +0000

    Navigation works through all screens

--- app/src/main/java/com/cyclepathy/AddEvent.java
+++ app/src/main/java/com/cyclepathy/AddEvent.java
@@ -7,7 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class AddEvent extends AppCompatActivity {
+public class AddEvent extends DrawerActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +16,6 @@ public class AddEvent extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
--- app/src/main/java/com/cyclepathy/Events.java
+++ app/src/main/java/com/cyclepathy/Events.java
@@ -1,13 +1,17 @@
 package com.cyclepathy;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class Events extends DrawerActivity {
+
+    Button addEvent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +20,15 @@ public class Events extends DrawerActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+     addEvent = (Button) findViewById(R.id.addEventButton);
+     addEvent.setOnClickListener(new View.OnClickListener(){
+         @Override
+         public void onClick(View view){
+             startActivity(new Intent(getApplicationContext(),AddEvent.class));
+         }
+     });
+
     }
+
 
 }
